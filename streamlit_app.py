65ac6cd31af3ff38529b7b52b855ed4776524cbc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-03 14:45:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ st.set_page_config(layout='wide')
 
 st.header('🎈 `st.line_chart` demo')
 
+st.subheader('Settings')
 plot_height = st.slider("Select the plot's height", 100, 500, 250)
 plot_data = st.multiselect(
      'Select data to plot',
      ['temp_min', 'temp_max', 'precipitation', 'wind'],
      ['temp_min', 'temp_max'])
-
 df = pd.read_csv('seattle-weather.csv', parse_dates=['date'])
 
+st.subheader('Plot')
 st.line_chart(df, x = 'date', y = plot_data, height = plot_height)
