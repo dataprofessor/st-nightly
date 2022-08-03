110c8bea111a8a5db1557e624c7456e24987da26
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-03 14:00:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.header('🎈 App Name')
 df = pd.read_csv('seattle-weather.csv', parse_dates=['date'])
 
 st.line_chart(df, x="date", y="temp_max") # plot one line
-st.line_chart(df, x="date", y=["temp_max", "temp_min"]) # plot multiple lines
+st.line_chart(df, x="date", y=["temp_max", "temp_min", "wind", "precipitation"]) # plot multiple lines
